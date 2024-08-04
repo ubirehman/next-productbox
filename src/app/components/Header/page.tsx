cb8230b168a46860d31f5a85596e38744ff9c8a5
Ubaid Ur Rehman <rehmanubaid5@gmail.com>
2024-08-04 19:56:12 +0500

    Relocated files

--- src/app/components/Header/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HeaderUI from "./pageUI";
-
-
-const Header = ({}) => {
-  return <HeaderUI />;
-};
-
-export default Header;
--- src/app/components/Hero/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HeroUI from "./pageUI";
-
-const Hero = ({}) => {
-  return <HeroUI />;
-};
-
-export default Hero;
