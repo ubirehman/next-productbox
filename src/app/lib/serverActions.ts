3e021e89e231ae037b6584257c1639a9d3d873ec
Ubaid Ur Rehman <rehmanubaid5@gmail.com>
2024-08-05 17:59:05 +0500

    casted values as string.

--- src/app/lib/serverActions.ts
+++ src/app/lib/serverActions.ts
@@ -3,8 +3,8 @@ import { createPost } from "../api/_controllers/posts";
 export async function createServerPost(formData: FormData) {
   "use server";
 
-  const title = formData.get("title");
-  const content = formData.get("content");
+  const title = formData.get("title") as string;
+  const content = formData.get("content") as string;
 
   const post = await createPost({
     title,
